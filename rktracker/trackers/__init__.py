23a5b4a8cd9055ac9fb94c88fc57181942dbac56
Brandon Castellano <brandon248@gmail.com>
2016-05-14 15:52:14 -0400

    Refactored file structure.

---
+++ rktracker/trackers/__init__.py
@@ -0,0 +1,12 @@
+
+import rktracker
+import rktracker.trackers
+import rktracker.trackers.tracker_python
+
+
+
+
+trackers.Tracker_Python = rktracker.trackers.tracker_python.Tracker_Python
+trackers.Tracker_Cpp = None
+trackers.Tracker_CUDA = None
+
---
+++ rktracker/trackers/tracker_python.py
@@ -0,0 +1,20 @@
+
+
+import rktracker
+import rktracker.trackers
+
+
+
+class Tracker_Python(object):
+
+    def __init__(self):
+        
+
+
+        
+        pass
+
+
+
+
+
